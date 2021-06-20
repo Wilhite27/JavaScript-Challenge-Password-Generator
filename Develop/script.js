95be6046132abe25804135d0b8e7308389276757
Dwilhite27 <wilhitedurrelle@gmail.com>
2021-06-19 19:57:35 -0500

    random string creation

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,14 @@
 // Assignment code here
+var characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()'
+
+function create_random_string() {
+var randomString = '';
+  for(var i = 0; i < characters.length; i++){
+    randomString += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  console.log(randomString);
+}
+create_random_string();
 
 
 // Get references to the #generate element
@@ -6,9 +16,9 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = create_random_string();
   var passwordText = document.querySelector("#password");
-
+ 
   passwordText.value = password;
 
 }
